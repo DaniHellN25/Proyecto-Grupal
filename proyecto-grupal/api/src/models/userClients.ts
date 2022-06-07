01e34846de60fcecc8aa7f8d92476ae46dc2a807
isagestoso <migdqmc@gmail.com>
2022-06-07 09:40:55 -0300

    feat(model/clients): add some mongoose types

--- proyecto-grupal/api/src/models/userClients.ts
+++ proyecto-grupal/api/src/models/userClients.ts
@@ -1,11 +1,17 @@
-import {prop, getModelForClass} from '@typegoose/typegoose'
+import {prop, getModelForClass, modelOptions} from '@typegoose/typegoose'
+
+@modelOptions({
+    schemaOptions: {
+        timestamps: true
+    }
+})
 
 class userClient {
     @prop({required: true})
     firstName: string
     @prop({required: true})
     lastName: string
-    @prop({required: true, trim: true})
+    @prop({required: true, trim: true, lowercase:true})
     email: string
     @prop({required: true, minlength: 8})
     password: string
